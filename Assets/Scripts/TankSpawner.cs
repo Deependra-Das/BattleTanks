e1e5d6234bb7484ca11af14ff8680c58da077ed2
Deependra-Das <iamdeep75@gmail.com>
2025-01-17 18:09:10 +0530

    Added Tank & Tank Spawner

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject tankPrefab;
+
+    void Start()
+    {
+        Instantiate(tankPrefab, transform.position, Quaternion.identity);
+    }
+
+
+}
