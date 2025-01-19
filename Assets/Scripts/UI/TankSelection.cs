a8087ea469cd851b70dacee8e5ad70d4796760d1
Deependra-Das <iamdeep75@gmail.com>
2025-01-19 22:52:36 +0530

    Implemented UI for Tank Selection

---
+++ Assets/Scripts/UI/TankSelection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelection : MonoBehaviour
+{
+    [SerializeField]
+    private TankSpawner _tankSpawner;
+
+    public void GreenTankSelected()
+    {
+       _tankSpawner.CreateTank(TankTypes.GreenTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void BlueTankSelected()
+    {
+        _tankSpawner.CreateTank(TankTypes.BlueTank);
+        this.gameObject.SetActive(false);
+    }
+
+    public void RedTankSelected()
+    {
+        _tankSpawner.CreateTank(TankTypes.RedTank);
+        this.gameObject.SetActive(false);
+    }
+}
